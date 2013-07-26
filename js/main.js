9b63083b0a6da67ec1929be1e586d9b9bad5087e
Carlos Pinelly <cpinelly@gmail.com>
2013-07-26 17:02:07 -0430

    Cambios de color en franjas

--- js/main.js
+++ js/main.js
@@ -1,32 +1,56 @@
 
 var contenidos;
-var activo = 0;
+var activo = -1;
+
+function cambiar_seccion() {
+		
+	posicion_actual = $(window).scrollTop();
+
+	n = contenidos.length;
+
+	var ant = 300;
+
+	for ( i = 0; i < n; i++){
+
+		if ( posicion_actual >= ant && posicion_actual < contenidos[i]['pos'] && activo != i){
+
+			contenidos[i]['elem'].animate({ backgroundColor: "green"}, 500);
+
+			if (activo != -1 )
+				contenidos[activo]['elem'].animate({ backgroundColor: "#ffa500"}, 500);
+
+			activo = i;
+
+			break;
+
+
+		}else{
+
+			console.log( i + ": "+  posicion_actual + ", " + ant + ", " + contenidos[i]['pos']);
+		}
+
+		ant = contenidos[i]['pos'];
+		
+	}
+}
 
 $(document).ready(function(){
 
 	contenidos = new Array();
 
-	$(".contenido").each(function(i, e){
+	$(".franja").each(function(i, e){
 
 		contenidos[i] = {};
 		contenidos[i]['elem'] = $(e);
-		contenidos[i]['pos'] = $(e).position().top;
-
-		//console.log(i);
-		//console.log(contenidos[i]);
+		contenidos[i]['pos'] = $(e).offset().top;		
 	});
+
+	
+	cambiar_seccion();
 
 	document.onscroll = function(event){
 
-		posicion_actual = $(window).scrollTop();
-
-		n = contenidos.length;
-
-		for ( i = 0; i < n; i++){
-
-			if ( posicion_actual+250 >= contenidos[i]['pos'] && activo != i)
-				activo = i;
-		}
+		cambiar_seccion();
 
 	}
 })
